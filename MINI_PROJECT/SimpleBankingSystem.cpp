b143831a35218e855022addec1fe5775bc4c70b6
Ansh Kaushik <1akiraforp@gmail.com>
2026-04-30 20:09:14 +0530

    Day 13

--- MINI_PROJECT/SimpleBankingSystem.cpp
+++ MINI_PROJECT/SimpleBankingSystem.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <map>
 using namespace std;
 
 class BankAccount {
@@ -11,12 +12,18 @@ private:
     double balance;
 
 public:
+    BankAccount() {} //A default constructor was needed for map
+
     BankAccount(string name, string id, string ifsc, long long accNum, double initialBalance) {
         accountHolderName = name;
         customerID = id;
         IFSC = ifsc;
         accountNumber = accNum;
         balance = (initialBalance >= 0) ? initialBalance : 0;
+    }
+
+    long long getAccountNumber() const {
+        return accountNumber;
     }
 
     void deposit(double amount) {
@@ -40,22 +47,116 @@ public:
     }
 
     void displayDetails() const {
-        cout << "\nAccount Holder: " << accountHolderName << endl;   
+        cout << "\nAccount Holder: " << accountHolderName << endl;
         cout << "Account Number: " << accountNumber << endl;
         cout << "Balance: " << balance << endl;
     }
-
-    double getBalance() const {
-        return balance;
-    }
 };
+
+void displayMenu(){
+    cout << "\nMenu:\n";
+    cout << "1. Create Account\n";
+    cout << "2. Deposit\n";
+    cout << "3. Withdraw\n";
+    cout << "4. Display Account Details\n";
+    cout << "5. Exit\n";
+}
 
 int main() {
     cout << fixed << setprecision(2);
-    BankAccount acc("John Doe", "C12345", "IFSC001", 1234567890, 1000.0);
-    acc.deposit(500);
-    acc.withdraw(200);
-    acc.withdraw(2000);
-    acc.displayDetails();
+
+    map<long long, BankAccount> accounts;
+    int choice;
+
+    do {
+        displayMenu();
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice) {
+
+            case 1: {
+                string name, id, ifsc;
+                long long accNum;
+                double balance;
+
+                cout << "Enter Name: ";
+                cin >> name;
+                cout << "Enter Customer ID: ";
+                cin >> id;
+                cout << "Enter IFSC: ";
+                cin >> ifsc;
+                cout << "Enter Account Number: ";
+                cin >> accNum;
+                cout << "Enter Initial Balance: ";
+                cin >> balance;
+
+                if (accounts.find(accNum) != accounts.end()) {
+                    cout << "Account already exists.\n";
+                } else {
+                    accounts[accNum] = BankAccount(name, id, ifsc, accNum, balance);
+                    cout << "Account created successfully.\n";
+                }
+                break;
+            }
+
+            case 2: {
+                long long accNum;
+                double amount;
+
+                cout << "Enter Account Number: ";
+                cin >> accNum;
+
+                if (accounts.find(accNum) != accounts.end()) {
+                    cout << "Enter amount to deposit: ";
+                    cin >> amount;
+                    accounts[accNum].deposit(amount);
+                } else {
+                    cout << "Account not found.\n";
+                }
+                break;
+            }
+
+            case 3: {
+                long long accNum;
+                double amount;
+
+                cout << "Enter Account Number: ";
+                cin >> accNum;
+
+                if (accounts.find(accNum) != accounts.end()) {
+                    cout << "Enter amount to withdraw: ";
+                    cin >> amount;
+                    accounts[accNum].withdraw(amount);
+                } else {
+                    cout << "Account not found.\n";
+                }
+                break;
+            }
+
+            case 4: {
+                long long accNum;
+
+                cout << "Enter Account Number: ";
+                cin >> accNum;
+
+                if (accounts.find(accNum) != accounts.end()) {
+                    accounts[accNum].displayDetails();
+                } else {
+                    cout << "Account not found.\n";
+                }
+                break;
+            }
+
+            case 5:
+                cout << "Exiting...\n";
+                break;
+
+            default:
+                cout << "Invalid choice.\n";
+        }
+
+    } while(choice != 5);
+
     return 0;
 }
